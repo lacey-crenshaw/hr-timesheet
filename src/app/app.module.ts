5059f9c75dc08b972d7f0aa29cd8d54a56480a0f
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-09-16 08:16:29 -0600

    Time Tracking App - Unit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DepartmentsComponent, TimesheetComponent, AnalyticsComponent, TopNavbarComponent, AnalyticsTableComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
