eacd5f2b27b31d5aebc8e14460ebd7fdfb3a78d6
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-09-16 09:16:14 -0600

    Time Sheet App - Unit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,23 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
-  declarations: [AppComponent, DepartmentsComponent, TimesheetComponent, AnalyticsComponent, TopNavbarComponent, AnalyticsTableComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
