cb8c509e208671de0436d6c262be354e47092e7f
lacey-crenshaw <lacey@laceycrenshaw.com>
2020-09-17 10:20:34 -0600

    Time Sheet App - Unit 4

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { AnalyticsComponent } from './../components/analytics/analytics.component';
+import { TimesheetComponent } from './../components/timesheet/timesheet.component';
+import { DepartmentsComponent } from './../components/departments/departments.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'timesheet', component: TimesheetComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
